57fb88ed240bd96a82fec397fca5c33b55cf62b5
HTF <roberthan1125@gmail.com>
2023-02-28 01:06:37 +0900

    :tutorial 1:20:00

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import styles from "./App.module.css";
+import { useState } from "react";
 
 const SingleAsset = (props) => {
   return (
     <div className={styles.name}>
-      <h1>
+      <h1 key={props.key}>
         {props.ticker} - {props.name}
       </h1>
     </div>
@@ -11,8 +12,16 @@ const SingleAsset = (props) => {
 };
 
 function App() {
-  const age = 18;
+
+  const [inputText, setInputText] = useState("");
+  const [age, setAge] = useState(15);
   const isGreen = true;
+  const increaseAge = () => {
+    setAge(age + 1);
+  };
+  const handleInputChange = (event) => {
+    setInputText(event.target.value);
+  };
 
   const assets = [
     { ticker: "SPY", name: "S&P500 ETF" },
@@ -22,11 +31,18 @@ function App() {
 
   return (
     <div className={styles.App}>
+      <input type="text" onChange={handleInputChange}></input>
+      {inputText}
       {age >= 18 ? <h1>Over Age</h1> : <h1>Under Age</h1>}
+      <button onClick={increaseAge}>Increase Age</button>
       <h1 style={{ color: isGreen ? "green" : "red" }}>This is Green</h1>
       {assets.map((asset, key) => {
         return (
-          <SingleAsset ticker={asset.ticker} name={asset.name}></SingleAsset>
+          <SingleAsset
+            ticker={asset.ticker}
+            name={asset.name}
+            key={key}
+          ></SingleAsset>
         );
       })}
     </div>
