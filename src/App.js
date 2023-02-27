2ddb09ad0addfc556516514b741b5d55bd79e280
HTF <roberthan1125@gmail.com>
2023-02-27 22:53:40 +0900

    :tutorial 1:00:00

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.css";
+
+const SingleAsset = (props) => {
+  return (
+    <div className={styles.name}>
+      <h1>
+        {props.ticker} - {props.name}
+      </h1>
+    </div>
+  );
+};
 
 function App() {
+  const age = 18;
+  const isGreen = true;
+
+  const assets = [
+    { ticker: "SPY", name: "S&P500 ETF" },
+    { ticker: "AGG", name: "AGGREGATE BOND ETF" },
+    { ticker: "TLT", name: "AGGREGATE BOND ETF" },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      {age >= 18 ? <h1>Over Age</h1> : <h1>Under Age</h1>}
+      <h1 style={{ color: isGreen ? "green" : "red" }}>This is Green</h1>
+      {assets.map((asset, key) => {
+        return (
+          <SingleAsset ticker={asset.ticker} name={asset.name}></SingleAsset>
+        );
+      })}
     </div>
   );
 }
