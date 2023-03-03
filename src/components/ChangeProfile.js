24460613bd39f49f151d62a258593ec45054fe55
Robert Han <roberthan1125@gmail.com>
2023-03-03 17:04:53 +0900

    4:45:00

--- src/components/ChangeProfile.js
+++ src/components/ChangeProfile.js
@@ -1,9 +1,14 @@
 import { useState } from "react";
 import { useContext } from "react";
+import { reach } from "yup";
 import { AppContext } from "../App";
+
+
+
+
 export const ChangeProfile = () => {
   const [newUsername, setNewUsername] = useState("");
-  const {setUsername} = useContext(AppContext);
+  const { setUsername } = useContext(AppContext);
   return (
     <div>
       <input
---
+++ src/components/app5.js
@@ -0,0 +1,43 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Profile } from "./pages/Profile";
+import { Contact } from "./pages/Contact";
+import { Register } from "./pages/Register";
+import { Navbar } from "./components/Navbar";
+import { useState, createContext } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+export const AppContext = createContext();
+
+function App() {
+  const [username, setUsername] = useState("Robert");
+  const client = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
+  return (
+    <div className="App">
+      <AppContext.Provider value={{ username, setUsername }}>
+        <QueryClientProvider client={client}>
+          <BrowserRouter>
+            <Navbar></Navbar>
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/profile" element={<Profile />}></Route>
+              <Route path="/contact" element={<Contact />}></Route>
+              <Route path="/register" element={<Register />}></Route>
+              <Route path="*" element={<h1>PAGE NOT FOUND</h1>}></Route>
+            </Routes>
+          </BrowserRouter>
+        </QueryClientProvider>
+      </AppContext.Provider>
+    </div>
+  );
+}
+
+export default App;
+
+
+// Quantum Computing: April Init research start.
